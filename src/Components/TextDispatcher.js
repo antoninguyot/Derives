e7e41d92a35fab77666aa7886765cc608a9b9e8d
Antonin <antonin.guyot@icloud.com>
2021-03-11 15:43:31 +0100

    hooks cleanup

--- src/Components/TextDispatcher.js
+++ src/Components/TextDispatcher.js
@@ -2,12 +2,9 @@ import React, {useEffect, useState} from 'react'
 import {StyleSheet, Text, View, TouchableOpacity} from 'react-native'
 
 import CCamera from './CCamera'
-import WeatherController from "../Helpers/WeatherController";
-import LocationController from "../Helpers/LocationController";
-import TimeController from "../Helpers/TimeController";
 import {TextGenerator, setTextArray, interpretText} from "../Helpers/TextGenerator";
-import SoundGenerator from "../Helpers/SoundGenerator";
-import {locationRequest, getLocation} from "../Helpers/locationRequests.js"
+import {locationRequest} from "../Helpers/locationRequests.js"
+import * as Location from "expo-location";
 
 
 const TextDispatcher = () => {
@@ -26,54 +23,61 @@ const TextDispatcher = () => {
     const [coefTextSpeed, setCoefTextSpeed] = useState(5)
     const [nbLines, setNbLines] = useState(4)
     const [debug, setDebug] = useState(false)
+    const [longitude, setLongitude] = useState()
+    const [latitude, setLatitude] = useState()
+    const [speed, setSpeed] = useState()
+    const [localityName, setLocalityName] = useState()
+    const [localityPopulation, setLocalityPopulation] = useState()
+    const [localitySurface, setLocalitySurface] = useState()
+    const [localityDensity, setLocalityDensity] = useState()
+    const [localityType, setLocalityType] = useState()
 
-    const [location2, setLocation2] = useState({
-        longitude: null,
-        latitude: null,
-        speed: null,
-        localityName: null,
-        localityPopulation: null,
-        localitySurface: null,
-        localityDensity: null,
-        localityType: null,
-    })
+    const updateLocation = (location) => {
+        // On ne veut faire une requête API que la première fois
+        let doLocationRequest = (latitude === undefined);
+
+        setLongitude(location.coords.longitude)
+        setLatitude(location.coords.latitude)
+        setSpeed(location.coords.speed)
+
+        if (doLocationRequest) {
+            locationRequest(location.coords.latitude, location.coords.longitude)
+                .then((function (response) {
+                    let locality = response.data[0];
+                    if (locality) {
+                        setLocalityName(locality.nom)
+                        setLocalityPopulation(locality.population)
+                        setLocalitySurface(locality.surface)
+                        setLocalityDensity(locality.population * 100 / locality.surface)
+                        setLocalityType(locality.population * 100 / locality.surface >= 376 ? 'city' : 'country')
+                    }
+                }))
+        }
+    }
 
 
     // equivalent du didMount
     useEffect(() => {
 
+        // Location
+        Location.requestPermissionsAsync()
+
+        Location.getCurrentPositionAsync().then(updateLocation)
+        // On update la position GPS en direct
+        Location.watchPositionAsync({
+            accuracy: Location.Accuracy.BestForNavigation,
+            distanceInterval: 1
+        }, updateLocation);
+
         // Séquence de démarrage de la vue texte
         setIsMounted(true)
-
-        // Initialisation des données des sensors
-        setLocation(getLocation(location2))
-        // LocationController.then((result) => setLocation(result)).catch((err) => console.log(err))
-        // WeatherController(location).then((result) => setWeather(result)).catch((err) => console.log(err))
-        // TimeController().then((result) => setTime(result)).catch((err) => console.log(err))
-        // this.state.time.initValues()
-        // this.state.location.initValues()
-        // this.state.weather.initValues()
-
-            
-        // Utilisation des données pour générer les poèmes
-        // TextGenerator.setTextArray(time).then((result) => setText(result)).catch((err) => console.log(err))
-        // SoundGenerator.startSound(time).then((result) => setSound(result)).catch((err) => console.log(err))
-
-        // Finalement on démarre le défilement du texte
-        // _startTimer()
-
-        // try{
-        //     const locationData = await locationRequest()
-        //     const weatherInformations = await getWeatherInformations(locationData)
-        // }catch(error){
-        // }
 
     }, [isMounted,setIsMounted])
 
       // Démarage du défilement du texte
       const _startTimer = () => {
         if (timerPaused) {
-            timerPaused = false
+            setTimerPaused(false)
             setTimer(setInterval(), coefTextSpeed * 1000)   
         }
       }
@@ -107,7 +111,7 @@ const TextDispatcher = () => {
                 <CCamera/>
             </View>
             <View style={styles.textContainer}>
-                <TouchableOpacity onLongPress={() => { debug = ! debug}}>
+                <TouchableOpacity onLongPress={() => { setDebug(! debug) }}>
                     <Text style={[styles.textOver, {fontSize: 20 * coefPolice}]}>
                         {vers}
                     </Text>
@@ -115,16 +119,16 @@ const TextDispatcher = () => {
             </View>
             {debug &&
             <View style={styles.containerCaptors}>
-                <Text style={styles.textCaptors}> Saison : {time.saison}  </Text>
-                <Text style={styles.textCaptors}> Moment : {time.moment}  </Text>
-                <Text style={styles.textCaptors}> Vitesse : {location.speed}  </Text>
-                <Text style={styles.textCaptors}> Latitude : {location.latitude}  </Text>
-                <Text style={styles.textCaptors}> Longitude : {location.longitude}  </Text>
-                <Text style={styles.textCaptors}> Ville : {location.localityName}  </Text>
-                <Text style={styles.textCaptors}> Densité de pop : {location.localityDensity} </Text>
-                <Text style={styles.textCaptors}> Milieu : {location.localityType}</Text>
-                <Text style={styles.textCaptors}> Météo : {weather.heat} </Text>
-                <Text style={styles.textCaptors}> Temperature : {weather.temperature}</Text>
+                {/*<Text style={styles.textCaptors}> Saison : {time.saison}  </Text>*/}
+                {/*<Text style={styles.textCaptors}> Moment : {time.moment}  </Text>*/}
+                <Text style={styles.textCaptors}> Vitesse : {speed}  </Text>
+                <Text style={styles.textCaptors}> Latitude : {latitude}  </Text>
+                <Text style={styles.textCaptors}> Longitude : {longitude}  </Text>
+                <Text style={styles.textCaptors}> Ville : {localityName}  </Text>
+                <Text style={styles.textCaptors}> Densité de pop : {localityDensity} </Text>
+                <Text style={styles.textCaptors}> Milieu : {localityType}</Text>
+                {/*<Text style={styles.textCaptors}> Météo : {weather.heat} </Text>*/}
+                {/*<Text style={styles.textCaptors}> Temperature : {weather.temperature}</Text>*/}
             </View>
             }
         </View>
