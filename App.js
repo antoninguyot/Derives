72238831393cbb65b08915e40f5b0648ecd71405
maylisdet <47695701+maylisdet@users.noreply.github.com>
2021-03-16 19:38:13 +0100

    Home Screen (#6)
    * Home Screen
    
    * weather (wip)
    
    Co-authored-by: Antonin <antonin.guyot@icloud.com>

--- App.js
+++ App.js
@@ -4,19 +4,19 @@ import Navigation from "./src/Navigation/Navigation";
 
 export default class AppTest extends React.Component {
 
-    render() {
-        return (
-            <Navigation/>
-        )
-    }
+  render() {
+    return (
+      <Navigation/>
+    )
+  }
 }
 
 const stylesNavigation = StyleSheet.create({
-    container: {
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        paddingTop: 20 //this amount should be equal to the header height so that any items displayed inside the container will start after the absolute positioned header
-    },
+  container: {
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    paddingTop: 20 //this amount should be equal to the header height so that any items displayed inside the container will start after the absolute positioned header
+  },
 });
--- src/Components/Accueil.js
+++ src/Components/Accueil.js
@@ -1,42 +1,36 @@
 import React from 'react'
 import { StyleSheet, View, Button, Text } from 'react-native'
 
+
 const description = "Vous allez vivre une expérience poétique – visuelle et sonore – en marchant.\nSelon votre vitesse, mais aussi le moment de la journée, la saison, la température, l'environnement, votre expérience ne sera pas la même..."
 
-class Accueil extends React.Component {
+const Accueil = ({navigation}) => {
 
-    _start(){
-        this.props.navigation.navigate('Texte')
-    }
-
-
-    render(){
-        return(
-            <View style={styles.mainContainer}>
-                <Text style={styles.welcomeText}>
-                    {description}
-                </Text>
-                <Button style={styles.buttonGo} title='GO' onPress={() => this._start()}/>
-            </View>
-        )
-    }
+  return(
+    <View style={styles.mainContainer}>
+      <Text style={styles.welcomeText}>
+        {description}
+      </Text>
+      <Button style={styles.buttonGo} title='GO' onPress={() => navigation.navigate('Menu')}/>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    welcomeText: {
-        fontSize: 28,
-        textAlign: "center",
-        marginBottom: 50,
-    },
-    buttonGo: {
-        width: 50,
-    },
-    mainContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        padding: 10,
-        backgroundColor: '#FFE7EE'
-    }
+  welcomeText: {
+    fontSize: 28,
+    textAlign: "center",
+    marginBottom: 50,
+  },
+  buttonGo: {
+    width: 50,
+  },
+  mainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#FFE7EE'
+  }
 })
 
 export default Accueil
