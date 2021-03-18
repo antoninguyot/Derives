fc7df4ac205ffbc11ab37f83dc764410860f778d
Antonin <antonin.guyot@icloud.com>
2021-03-18 19:43:44 +0100

    Word generation cleanup

--- src/Helpers/text.js
+++ src/Helpers/text.js
@@ -31,7 +31,7 @@ export const getTextArray = (moment) => {
  * @param saison
  * @param heat
  */
-export const interpretText = (sentence, localityType, speed, saison, heat) => {
+export const interpretText = (sentence, localityType, activity, saison, heat) => {
     let sentence_new = ""
     for (let i = 0; i < sentence.length; i++) {
         if (sentence.charAt(i) === "$") {
@@ -44,26 +44,16 @@ export const interpretText = (sentence, localityType, speed, saison, heat) => {
                     sentence_new += tab[Math.floor((Math.random() * tab.length))]
                     break
                 case "G":
-                    if (localityType === "rural") sentence_new += tab.rural[Math.floor((Math.random() * tab.rural.length))]
-                    if (localityType === "urbain") sentence_new += tab.urbain[Math.floor((Math.random() * tab.urbain.length))]
+                    sentence_new += tab[localityType][Math.floor((Math.random() * tab[localityType].length))]
                     break
                 case "V":
-                    if (speed < 2) sentence_new += tab.stationary[Math.floor((Math.random() * tab.stationary.length))]
-                    else if (speed < 6.4) sentence_new += tab.walking[Math.floor((Math.random() * tab.walking.length))]
-                    else if (speed < 8) sentence_new += tab.running[Math.floor((Math.random() * tab.running.length))]
-                    else if (speed < 30) sentence_new += tab.cycling[Math.floor((Math.random() * tab.cycling.length))]
-                    else  sentence_new += tab.in_vehicle[Math.floor((Math.random() * tab.in_vehicle.length))]
+                    sentence_new += tab[activity][Math.floor(Math.random() * tab[activity].length)]
                     break
                 case "T":
-                    if (heat === "cold") sentence_new += tab.cold[Math.floor((Math.random() * tab.cold.length))]
-                    if (heat === "sweet") sentence_new += tab.cold[Math.floor((Math.random() * tab.sweet.length))]
-                    if (heat === "hot") sentence_new += tab.cold[Math.floor((Math.random() * tab.hot.length))]
+                    sentence_new += tab[heat][Math.floor((Math.random() * tab[heat].length))]
                     break
                 case "S":
-                    if (saison === "printemps") sentence_new += tab.printemps[Math.floor((Math.random() * tab.printemps.length))]
-                    if (saison === "été") sentence_new += tab.été[Math.floor((Math.random() * tab.été.length))]
-                    if (saison === "automne") sentence_new += tab.automne[Math.floor((Math.random() * tab.automne.length))]
-                    if (saison === "hiver") sentence_new += tab.hiver[Math.floor((Math.random() * tab.hiver.length))]
+                    sentence_new += tab[saison][Math.floor((Math.random() * tab[saison].length))]
                     break
                 default:
                     sentence_new += "ERROR"
@@ -75,7 +65,7 @@ export const interpretText = (sentence, localityType, speed, saison, heat) => {
     }
     for (let i = 0; i < sentence_new.length; i++) {
         if (sentence_new.charAt(i) === "$") {
-            sentence_new = interpretText(sentence_new, localityType, speed, saison, heat) //Permet de faire 2 niveaux d'interprétation
+            sentence_new = interpretText(sentence_new, localityType, activity, saison, heat) //Permet de faire 2 niveaux d'interprétation
         }
     }
     return sentence_new
