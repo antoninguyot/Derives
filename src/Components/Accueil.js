e4db6802aa02f7adde83ede5e93cb44e4cb63c0e
maylisdet <47695701+maylisdet@users.noreply.github.com>
2021-03-18 15:25:12 +0100

    mayo is the best (#7)

--- src/Components/Accueil.js
+++ src/Components/Accueil.js
@@ -1,17 +1,43 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { StyleSheet, View, Button, Text } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const description = "Vous allez vivre une expérience poétique – visuelle et sonore – en marchant.\nSelon votre vitesse, mais aussi le moment de la journée, la saison, la température, l'environnement, votre expérience ne sera pas la même..."
 
 const Accueil = ({navigation}) => {
+  const [firstConnexion, setFirstConnexion] = useState()
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('firstConnexionDate')
+      if(value !== null) {
+        setFirstConnexion(false)
+      } else {
+        setFirstConnexion(true) 
+        const jsonValue = JSON.stringify(new Date)
+        await AsyncStorage.setItem('firstConnexionDate', jsonValue)
+      }
+    } catch(e) {
+      console.log("erreur", e)
+    }
+  }
+
+  useEffect( () => {
+    getData()
+  }, [])
 
   return(
     <View style={styles.mainContainer}>
       <Text style={styles.welcomeText}>
         {description}
       </Text>
-      <Button style={styles.buttonGo} title='GO' onPress={() => navigation.navigate('Menu')}/>
+      {firstConnexion && (
+        <Button style={styles.buttonGo} title='GO' onPress={() => navigation.navigate('Texte')}/>
+      )}
+      {!firstConnexion &&
+      (<Button style={styles.buttonGo} title='GO' onPress={() => navigation.navigate('Menu')}/>
+      )}
     </View>
   )
 }
