1175d68e8d71de51450e16c3a8f4aacc25b6ea74
Antonin <antonin.guyot@icloud.com>
2021-03-20 16:47:51 +0100

    passage au prochain moment si accélération dans le sas

--- src/Components/Sas.js
+++ src/Components/Sas.js
@@ -2,8 +2,9 @@ import React, {useEffect, useState} from 'react'
 import {Button, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import CCamera from "./CCamera";
 import * as Location from "expo-location";
+import {calculateMoment} from "../Helpers/time";
 
-const Sas = ({ navigation }) => {
+const Sas = ({navigation}) => {
 
     const [initialSpeed, setInitialSpeed] = useState(0)
     const [speedAverage, setSpeedAverage] = useState(0)
@@ -27,16 +28,29 @@ const Sas = ({ navigation }) => {
 
         // Set timeout for n seconds
         let sasTimeout = setTimeout(() => {
-            if(speedAverage - initialSpeed > 0){
-                navigation.replace('Texte',{
-                    moment: 'matin'
+            if (speedAverage - initialSpeed > 0) {
+                let newMoment = () => {
+                    let moment = calculateMoment()
+                    switch (moment) {
+                        case 'nuit':
+                            return 'matin'
+                        case 'matin':
+                            return 'midi'
+                        case 'midi':
+                            return 'soir'
+                        case 'soir':
+                            return 'nuit'
+                    }
+                }
+                navigation.replace('Texte', {
+                    moment: newMoment()
                 })
             } else {
                 navigation.replace('Texte')
             }
         }, 10000)
-            // If the avg speed was higher than the original, navigate to the next period
-            // Else, loop
+        // If the avg speed was higher than the original, navigate to the next period
+        // Else, loop
 
         return () => {
             clearTimeout(sasTimeout)
